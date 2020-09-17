0f5249bb3220d83170a40db8738751e973cd46d3
nsolan <neeti269@gmail.com>
2020-09-18 01:12:46 +0530

    code change commit to master

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test
 {
 	public static void main(String args[])
 	{
-		System.out.println("This is GIT test project");
+		System.out.println("This is GIT test project with branch");
+
 	}
 }
